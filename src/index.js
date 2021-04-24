fb0413d19e4434bb6440eeba880ecc6f4503598d
fgiannotti <franco.giannotti@mercadolibre.com>
2021-04-24 20:16:45 -0300

    add webpack dev

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 
 
 const Index = () => {
-    return <div>WELCOME TO REACT APP!!!!!!</div>;
+    return <div>WELCOME TO REACT APP!</div>;
 };
 
-
-ReactDOM.render(<Index /> ,document.getElementById('root'));
+ReactDOM.render(<Index /> ,document.getElementById('app'));
