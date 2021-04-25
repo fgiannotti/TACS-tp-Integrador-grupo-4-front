3c16d632692081d580b056d1f726cb670e07924e
fgiannotti <franco.giannotti@mercadolibre.com>
2021-04-25 18:24:27 -0300

    unadded files

---
+++ src/components/Google.jsx
@@ -0,0 +1,23 @@
+
+import GoogleLogin from 'react-google-login';
+
+
+const responseGoogle = (response) => {
+    console.log(response);
+  }
+
+  
+const GoogleSignIn = () => {
+    return (
+        <GoogleLogin
+        clientId='1058494963753-drauquf06tsu1jnbl7k13ptrp98s323d.apps.googleusercontent.com'
+        buttonText="Sign in with Google"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+        isSignedIn={true}
+    />
+  )
+}
+
+export default GoogleSignIn;
