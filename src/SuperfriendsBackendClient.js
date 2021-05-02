fd2f028993f829c5d238ab255e25a19a33698401
JulietaAbuin <julieta.abuin@despegar.com>
2021-05-02 01:07:03 -0300

    added deck crud

--- src/SuperfriendsBackendClient.js
+++ src/SuperfriendsBackendClient.js
@@ -21,6 +21,24 @@ class SuperfriendsBackendClient {
             .catch((e) => console.log("Error fetching decks: " + e))
     }
 
+    createDeck = (createDeckDTO) => {
+        return axios.post(this.backendUrl + "/decks", createDeckDTO)
+            .then((response) => console.log(response))
+            .catch((e) => console.log("Error creating deck with info :" + createDeckDTO + "   " + e))
+    }
+
+    updateDeck = (deckId, updateDeckDTO) => {
+        return axios.put(this.backendUrl + "/decks/" + deckId, updateDeckDTO)
+            .then((response) => console.log(response))
+            .catch((e) => console.log("Error updating deck with id :" + deckId + "   " + e))
+    }
+
+    deleteDeck = (deckId) => {
+        return axios.delete(this.backendUrl + "/decks/" + deckId)
+            .then((response) => console.log(response))
+            .catch((e) => console.log("Error deleting deck with id :" + deckId + "   " + e))
+    }
+
 }
 
 export default SuperfriendsBackendClient;
