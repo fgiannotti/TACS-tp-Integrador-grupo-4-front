0be0f79be831ca063beec6aaf50d0da95e905762
fgiannotti <franco.giannotti@mercadolibre.com>
2021-05-02 14:04:58 -0300

    add logout component with redirect after click

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import GoogleSignOut from '../login/GoogleSignOut';
 
 class Home extends React.Component {
     render() {
         return (
             <div style={{backgroundColor:'#ffcc80'}}>
                 <h1>Homeee</h1>
+                <GoogleSignOut/>
                 {this.props.isAdmin ? <div style={{backgroundColor:'#ffcc88',minHeight:'300px'}}/>: <React.Fragment/>}
             </div>
         )
