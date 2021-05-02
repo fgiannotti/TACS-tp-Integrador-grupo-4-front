174e2671accc08b237b3cc520466352476d7bdb1
JulietaAbuin <julieta.abuin@despegar.com>
2021-05-02 01:01:50 -0300

    added fetch card by name and decks

--- src/SuperfriendsBackendClient.js
+++ src/SuperfriendsBackendClient.js
@@ -4,11 +4,22 @@ class SuperfriendsBackendClient {
     backendUrl = "http://localhost:9000"
 
     postLogin = (userInfoDTO) => {
-        return axios.post(this.backendUrl +"/login", userInfoDTO)
+        return axios.post(this.backendUrl + "/login", userInfoDTO)
             .then((response) => response.data)
-            .catch((e) => console.log("Error fetching user information to backend in login" + e.toString()))
+            .catch((e) => console.log("Error fetching user information in login: " + e))
     }
 
+    getCardsByName = (name) => {
+        return axios.get(this.backendUrl + "/cards?name=" + name)
+            .then((response) => response.data)
+            .catch((e) => console.log("Error fetching cards by name: " + e))
+    }
+
+    getDecks = () => {
+        return axios.get(this.backendUrl + "/decks")
+            .then((response) => response.data)
+            .catch((e) => console.log("Error fetching decks: " + e))
+    }
 
 }
 
