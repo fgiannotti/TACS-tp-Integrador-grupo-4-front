68fec75ce8539b8bcd15ad9e688b6ec407ca59bb
JulietaAbuin <julieta.abuin@despegar.com>
2021-05-30 13:01:04 -0300

    remove unused imports in game

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,18 +1,8 @@
-import React, {useEffect} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardMedia from "@material-ui/core/CardMedia";
 import Button from '@material-ui/core/Button';
 import Avatar from '@material-ui/core/Avatar';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Dialog from '@material-ui/core/Dialog';
-import {ButtonGroup} from "@material-ui/core";
-import IconButton from "@material-ui/core/IconButton";
-import MediaCard from "../cards/HeroCard";
-import DialogContentText from '@material-ui/core/DialogContentText';
-import { withSnackbar } from "./GameSnackBar";
+import {withSnackbar} from "./GameSnackBar";
 import {Redirect} from "react-router"
 import ManagementSocket from "../management_socket/ManagementSocket";
 import styles from '../../styles/Game.css';
